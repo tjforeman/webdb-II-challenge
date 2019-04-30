2df416c36322af2bb8774e18eb32e922306d14fd
tjforeman <tylerforeman92@gmail.com>
2019-04-30 15:54:12 -0400

    get by id and delete working

--- data/zoo-router.js
+++ data/zoo-router.js
@@ -35,5 +35,39 @@ router.get('/', (req, res) => {
     })
   });
 
+  router.get('/:id', (req, res) => {
+    db('zoos')
+    .where({id:req.params.id})
+    .first()
+    .then(zoo=>{
+      if(zoo){
+      res.status(200).json(zoo)
+      }else{
+        res.status(404).json({message:'the specified zoo does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json(err)
+    })
+  });
+
+  router.delete('/:id', (req, res) => {
+    db('zoos')
+    .where({id:req.params.id})
+    .del()
+    .then(count =>{
+      if (count>0){
+        res.status(200).json({message:`${count} record was deleted`})
+      }else{
+        res.status(400).json({message:'the specified zoo does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json(err)
+    })
+  });
+
+
+
 
 module.exports = router;

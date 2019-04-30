8cee4c814852dc9f808ebb8629d221306ba4e083
tjforeman <tylerforeman92@gmail.com>
2019-04-30 15:43:21 -0400

    post request working

--- data/zoo-router.js
+++ data/zoo-router.js
@@ -11,7 +11,20 @@ useNullAsDefault:true,
 
 const db =knex(knexConfig);
 
-
+router.post('/', (req, res) => {
+    if (!req.body.name){
+        res.status(400).json({message:'A name is required to add a Zoo'})
+    }else{
+    db('zoos')
+    .insert(req.body,'id')
+    .then(results =>{
+      res.status(200).json(results)
+    })
+    .catch(err=>{
+      res.status(500).json(err)
+    })
+}
+  });
 
 router.get('/', (req, res) => {
     db('zoos')
